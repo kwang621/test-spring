715340be0a27a274ff97c642bfed42a560bb0154
kwang621 <kwang@resquare.co.kr>
2022-04-14 20:03:02 +0900

    test upload

---
+++ src/main/java/test/testspring/controller/HelloController.java
@@ -0,0 +1,21 @@
+package test.testspring.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+//    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("test")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        log.info("test1");
+        return "test";
+    }
+}
